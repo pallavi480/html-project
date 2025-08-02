e941181a64bbe05773f3d0bf59a3a1b86fae79e2
pallavi gayaki <pallavigayaki@gmail.com>
2025-08-02 15:35:59 +0530

    added my javascript basic code
    Create a new branch for this commit and start a pull request

---
+++ prob1.js
@@ -0,0 +1,19 @@
+
+let a = "dress"
+let b = "shoes"
+let c = "computer"
+
+console.log(a)
+console.log(b)
+console.log("how are you",c,"my name is pallavi")
+
+let x = 10; //number
+let y = "34" //string
+// console.log (typeof x)
+
+let yes = true; //boolean
+console.log(typeof yes)
+
+let abc = false;
+console.log(typeof abc)
+
---
+++ prob2.js
@@ -0,0 +1,49 @@
+//Arithmetic operator + - * / % **
+
+// let a = 5;
+// let b = 10;
+// let c = b-a;
+
+// console.log("output is",a+b,"ok")
+
+// console.log("result is",c)
+
+// console.log(a*b)
+
+// console.log(b/a)/
+
+// modulo operator % = reminder
+
+// let x = 9;
+// let y = 2;
+// let a = 5;
+// let b = 3;
+
+// console.log(x%y)
+
+// console.log(a%b)
+
+// exponational operator ** = power 
+
+// let x = 2;
+// let y = 4;
+
+// let z = x**y // x's power y, 2*2*2*2*
+
+// console.log(z)
+
+// console.log (x**5)//2*2*2*2*2
+
+// console.log (7**4)//7*7*7*7
+
+
+let a = 5;
+let b = 2;
+
+let c = 2**3 //*2*2*2
+
+let d = a**b //*5*5*5
+
+console.log(d)
+
+
